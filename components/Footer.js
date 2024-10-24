460a864643ec429a94c2c3fd0db49f56bc84c59e
meskatabusayed <meskatabusayed@gmail.com>
2024-10-24 23:07:19 +0530

    simple footer added

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+// components/Footer.js
+const Footer = () => {
+    return (
+      <footer>
+        <p>&copy; {new Date().getFullYear()} Your Name. All rights reserved.</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
